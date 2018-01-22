87daeb5cfb05b0db3437cbdbbbed83b8c251caab
Stanislav Petrov <s.e.petrov@gmail.com>
2018-01-22 14:21:22 +0000

    removed exported properties in vafour of exported methods; added Card factory; added AuthorizationRequest factory

--- pkg/internal/model/model.go
+++ pkg/internal/model/model.go
@@ -11,29 +11,72 @@ import (
 
 // AuthorizationRequest represents the requests sent by a merchant to charge a customer.
 type AuthorizationRequest struct {
-	UUID           uuid.UUID
-	CardUUID       uuid.UUID
-	MerchantUUID   uuid.UUID
-	BlockedAmount  uint64
-	CapturedAmount uint64
-	RefundedAmount uint64
-	History        []AuthorizationRequestSnapshot
+	uuid           uuid.UUID
+	cardUUID       uuid.UUID
+	merchantUUID   uuid.UUID
+	blockedAmount  uint64
+	capturedAmount uint64
+	refundedAmount uint64
+	history        []AuthorizationRequestSnapshot
+}
+
+// NewAuthorizationRequest creates new AuthorizationRequest if the request is authorised. It returns an error if the
+// request is not authorised.
+func NewAuthorizationRequest(card Card, merchant uuid.UUID, amount uint64) (*AuthorizationRequest, error) {
+	if card.AvailableBalance() < amount {
+		return &AuthorizationRequest{}, errors.New("available balance is too low")
+	}
+	if card.BlockedBalance() > math.MaxUint64-amount {
+		return &AuthorizationRequest{}, errors.New("blocked balance cannot exceed math.MaxUint64")
+	}
+	snapshot := AuthorizationRequestSnapshot{
+		uuid:          uuid.NewV4(),
+		blockedAmount: amount,
+		createdAt:     time.Now(),
+	}
+	return &AuthorizationRequest{
+		uuid:          uuid.NewV4(),
+		cardUUID:      card.UUID(),
+		merchantUUID:  merchant,
+		blockedAmount: amount,
+		history:       []AuthorizationRequestSnapshot{snapshot},
+	}, nil
 }
 
 // AuthorizationRequestSnapshot represents a snapshot of AuthorizationRequest.
 type AuthorizationRequestSnapshot struct {
-	UUID           uuid.UUID
-	BlockedAmount  uint64
-	CapturedAmount uint64
-	RefundedAmount uint64
-	CreatedAt      time.Time
+	uuid           uuid.UUID
+	blockedAmount  uint64
+	capturedAmount uint64
+	refundedAmount uint64
+	createdAt      time.Time
 }
 
 // Card represents a prepaid card.
 type Card struct {
-	UUID             uuid.UUID
-	AvailableBalance uint64
-	BlockedBalance   uint64
+	uuid             uuid.UUID
+	availableBalance uint64
+	blockedBalance   uint64
+}
+
+// NewCard returns new Card.
+func NewCard() *Card {
+	return &Card{uuid: uuid.NewV4()}
+}
+
+// UUID returns the UUID.
+func (c *Card) UUID() uuid.UUID {
+	return c.uuid
+}
+
+// AvailableBalance returns the available balance.
+func (c *Card) AvailableBalance() uint64 {
+	return c.availableBalance
+}
+
+// BlockedBalance returns the blocked balance.
+func (c *Card) BlockedBalance() uint64 {
+	return c.blockedBalance
 }
 
 // LoadMoney loads amount onto c.
@@ -41,10 +84,10 @@ func (c *Card) LoadMoney(amount uint64) error {
 	if amount == 0 {
 		return errors.New("amount must be greater than zero")
 	}
-	if c.AvailableBalance > math.MaxUint64-amount {
+	if c.availableBalance > math.MaxUint64-amount {
 		return errors.New("available balance cannot exceed math.MaxUint64")
 	}
-	c.AvailableBalance += amount
+	c.availableBalance += amount
 	return nil
 }
 
@@ -53,11 +96,11 @@ func (c *Card) BlockMoney(amount uint64) error {
 	if amount == 0 {
 		return errors.New("amount must be greater than zero")
 	}
-	if amount > c.AvailableBalance {
+	if amount > c.availableBalance {
 		return errors.New("available balance is too low")
 	}
-	c.AvailableBalance -= amount
-	c.BlockedBalance += amount
+	c.availableBalance -= amount
+	c.blockedBalance += amount
 	return nil
 }
 
@@ -66,22 +109,22 @@ func (c *Card) ChargeMoney(amount uint64) error {
 	if amount == 0 {
 		return errors.New("amount must be greater than zero")
 	}
-	if amount > c.BlockedBalance {
+	if amount > c.blockedBalance {
 		return errors.New("blocked balance is too low")
 	}
-	c.BlockedBalance -= amount
+	c.blockedBalance -= amount
 	return nil
 }
 
 // Transaction represents a transaction associated with a card.
 type Transaction struct {
-	UUID             uuid.UUID
-	CardUUID         uuid.UUID
-	EventUUID        uuid.UUID
-	EventType        string
-	Date             time.Time
-	Amount           uint64
-	AvailableBalance uint64
-	BlockedBalance   uint64
-	Description      string
+	uuid             uuid.UUID
+	cardUUID         uuid.UUID
+	eventUUID        uuid.UUID
+	eventType        string
+	date             time.Time
+	amount           uint64
+	availableBalance uint64
+	blockedBalance   uint64
+	description      string
 }
--- pkg/internal/model/model_test.go
+++ pkg/internal/model/model_test.go
@@ -3,36 +3,37 @@ package model
 import (
 	"math"
 	"testing"
-
-	"github.com/satori/go.uuid"
 )
 
 func TestCard_LoadMoney(t *testing.T) {
 	t.Run("amount must be greater than zero", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4()}
+		c := NewCard()
 		if c.LoadMoney(0) == nil {
 			t.Error("c.LoadBalance(0) nil; want error")
 		}
 	})
 	t.Run("available balance cannot become greater than math.MaxUint64", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4(), AvailableBalance: 1}
+		c := NewCard()
+		if err := c.LoadMoney(1); err != nil {
+			t.Fatalf("c.LoadMoney(1) err; want nil; %v", err)
+		}
 		if c.LoadMoney(math.MaxUint64) == nil {
 			t.Error("c.LoadMoney(math.MaxUint64) nil; want error")
 		}
 	})
 	t.Run("available balance can reach math.MaxUint64", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4()}
+		c := NewCard()
 		if err := c.LoadMoney(math.MaxUint64); err != nil {
 			t.Errorf("c.LoadMoney(math.MaxUint64) %v; want nil", err)
 		}
-		if c.AvailableBalance != math.MaxUint64 {
-			t.Errorf("c.AvailableBalance == %+v; want math.MaxUint64", c.AvailableBalance)
+		if c.AvailableBalance() != math.MaxUint64 {
+			t.Errorf("c.AvailableBalance() == %+v; want math.MaxUint64", c.AvailableBalance())
 		}
-		if c.BlockedBalance != 0 {
-			t.Errorf("c.BlockedBalance == %+v; want 0", c.BlockedBalance)
+		if c.BlockedBalance() != 0 {
+			t.Errorf("c.BlockedBalance() == %+v; want 0", c.BlockedBalance())
 		}
 	})
 }
@@ -40,30 +41,36 @@ func TestCard_LoadMoney(t *testing.T) {
 func TestCard_BlockMoney(t *testing.T) {
 	t.Run("amount must be greater than zero", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4()}
+		c := NewCard()
 		if c.BlockMoney(0) == nil {
 			t.Error("c.BlockMoney(0) nil; want error")
 		}
 	})
 	t.Run("available balance cannot become negative", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4(), AvailableBalance: 13}
+		c := NewCard()
+		if err := c.LoadMoney(13); err != nil {
+			t.Fatalf("c.LoadMoney(13) err; want nil; %v", err)
+		}
 		if c.BlockMoney(14) == nil {
 			t.Error("c.LoadMoney(math.MaxUint64) nil; want error")
 		}
 	})
 	t.Run("available balance can become zero", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4(), AvailableBalance: 13}
+		c := NewCard()
+		if err := c.LoadMoney(13); err != nil {
+			t.Fatalf("c.LoadMoney(13) err; want nil; %v", err)
+		}
 		if c.BlockMoney(13) != nil {
 			t.Fatalf("c.LoadMoney(math.MaxUint64) error; want nil")
 		}
-		if c.AvailableBalance != 0 {
-			t.Errorf("c.AvailableBalance == %+v; want 0", c.AvailableBalance)
+		if c.AvailableBalance() != 0 {
+			t.Errorf("c.AvailableBalance() == %+v; want 0", c.AvailableBalance())
 		}
 
-		if c.BlockedBalance != 13 {
-			t.Errorf("c.BlockedBalance == %+v; want 13", c.BlockedBalance)
+		if c.BlockedBalance() != 13 {
+			t.Errorf("c.BlockedBalance() == %+v; want 13", c.BlockedBalance())
 		}
 	})
 }
@@ -71,30 +78,42 @@ func TestCard_BlockMoney(t *testing.T) {
 func TestCard_ChargeMoney(t *testing.T) {
 	t.Run("cannot charge 0", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4()}
+		c := NewCard()
 		if c.ChargeMoney(0) == nil {
 			t.Error("c.ChargeMoney(0) nil; want error")
 		}
 	})
 	t.Run("cannot charge more than the blocked balance", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4(), BlockedBalance: 2}
+		c := NewCard()
+		if err := c.LoadMoney(5); err != nil {
+			t.Fatalf("c.LoadMoney(5) err; want nil; %v", err)
+		}
+		if err := c.BlockMoney(2); err != nil {
+			t.Fatalf("c.BlockMoney(2) nil; want error; %v", err)
+		}
 		if c.ChargeMoney(3) == nil {
 			t.Error("c.ChargeMoney(3) nil; want error")
 		}
 	})
 	t.Run("blocked balance can become zero", func(t *testing.T) {
 		t.Parallel()
-		c := &Card{UUID: uuid.NewV4(), AvailableBalance: 3, BlockedBalance: 4}
-		if c.ChargeMoney(4) != nil {
-			t.Fatalf("c.ChargeMoney(13) error; want nil")
+		c := NewCard()
+		if err := c.LoadMoney(5); err != nil {
+			t.Fatalf("c.LoadMoney(5) err; want nil; %v", err)
 		}
-		if c.AvailableBalance != 3 {
-			t.Errorf("c.AvailableBalance == %+v; want 0", c.AvailableBalance)
+		if err := c.BlockMoney(4); err != nil {
+			t.Fatalf("c.BlockMoney(4) nil; want error; %v", err)
+		}
+		if c.ChargeMoney(4) != nil {
+			t.Fatalf("c.ChargeMoney(4) error; want nil")
+		}
+		if c.AvailableBalance() != 1 {
+			t.Errorf("c.AvailableBalance() == %+v; want 0", c.AvailableBalance())
 		}
 
-		if c.BlockedBalance != 0 {
-			t.Errorf("c.BlockedBalance == %+v; want 13", c.BlockedBalance)
+		if c.BlockedBalance() != 0 {
+			t.Errorf("c.BlockedBalance() == %+v; want 13", c.BlockedBalance())
 		}
 	})
 }
